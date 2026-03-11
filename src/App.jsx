8e41a2e055d35a0a15231ed240b7d08824a9a6ce
salv-ritrovato <salv.ritrovato@gmail.com>
2026-03-11 14:09:38 +0100

    working on task

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AppHeader from "./components/AppHeader"
+import AccordionTest from './components/AccordionTest';
+
 
 function App() {
 
@@ -6,6 +10,7 @@ function App() {
   return (
     <>
       <AppHeader />
+      <AccordionTest />
     </>
   )
 }
